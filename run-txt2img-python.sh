59e8c541ad3d7195cfb31ce0e73e65b3ac3efa9e
Ean Garvey <ean.garvey@amd.com>
2024-05-01 18:55:50 -0500

    Update t2i runner

--- run-txt2img-python.sh
+++ run-txt2img-python.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+set -xeu
+
 if [ $# -ne 5 ]; then
     echo "Usage: $0 <prompt> <neg_prompt> <batch_count> <device_id> <weights_path>"
     exit 1
 fi
 
-prompt="$1"
-neg_prompt="$2"
-batch_count="$3"
-device="$4"
-weights_path="$5"
+prompt="--prompt=$1"
+neg_prompt="--negative_prompt=$2"
+batch_count="--batch_count=$3"
+device="rocm://$4"
+weights_path="--external_weights_dir=$5"
 
 python3 ../SHARK-Turbine/models/turbine_models/custom_models/sdxl_inference/sdxl_compiled_pipeline.py \
   --precision=fp16 \
@@ -20,11 +22,11 @@ python3 ../SHARK-Turbine/models/turbine_models/custom_models/sdxl_inference/sdxl
   --scheduler_id=PNDM \
   --num_inference_steps=30 \
   --pipeline_dir=./tmp/ \
-  --external_weights_dir=$weights_path \
+  $weights_path \
   --attn_spec=default  \
-  --batch_count=$batch_count \
-  --prompt=$prompt \
-  --negative_prompt=$neg_prompt | tee output.txt
+  $batch_count \
+  $prompt \
+  $neg_prompt | tee output.txt
 
 grep -oE '[^/]+\.png' output.txt > filenames.txt
 
